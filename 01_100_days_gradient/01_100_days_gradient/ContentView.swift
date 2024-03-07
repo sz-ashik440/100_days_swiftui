66a36a5ba2fd846d961d3b846f09807ac27f912e
sz ashik <szashik440@gmail.com>
2024-03-07 22:02:47 +0600

    initial commit

---
+++ 01_100_days_gradient/01_100_days_gradient/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  01_100_days_gradient
+//
+//  Created by sz ashik on 7/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    VStack {
+      Text("100 Days of SwiftUI!")
+    }
+    .padding()
+  }
+}
+
+#Preview {
+  ContentView()
+}
---
+++ 01_100_days_gradient/01_100_days_gradient/_1_100_days_gradientApp.swift
@@ -0,0 +1,17 @@
+//
+//  _1_100_days_gradientApp.swift
+//  01_100_days_gradient
+//
+//  Created by sz ashik on 7/3/24.
+//
+
+import SwiftUI
+
+@main
+struct _1_100_days_gradientApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
